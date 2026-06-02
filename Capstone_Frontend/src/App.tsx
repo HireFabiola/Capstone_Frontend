778bd754aa9cd40d70cbfe766a0d446132ad2ad0
Fabiola <fabiola.aurelien@gmail.com>
2026-06-02 14:31:20 -0400

    Added logic to wire up App.tsx to the pages.  Also, corrected file misspelling

--- Capstone_Frontend/src/App.tsx
+++ Capstone_Frontend/src/App.tsx
@@ -1,13 +1,42 @@
+// Importing necessary libraries and components
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
+// Importing page components for the public website
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";
+import WorkPage from "./pages/WorkPage";
+import ContactPage from "./pages/ContactPage";
+
+// Importing page components for the admin dashboard  
+import AdminLoginPage from "./pages/AdminLoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import InquiriesPage from "./pages/InquiriesPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import TasksPage from "./pages/TasksPage";
+
 
 function App() {
- 
   return (
-    <>
-    Here we go with the frontend part of the project. We will be using React and Vite to build our application. This is just a starting point, and we will be adding more features and components as we progress. Stay tuned for more updates!
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Public Website routes */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/work" element={<WorkPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+
+        {/* Admin Website routes */}
+        <Route path="/admin/login" element={<AdminLoginPage />}/>
+        <Route path="/admin/dashboard" element={<DashboardPage />}/>
+        <Route path="/admin/inquiries" element={<InquiriesPage />}/>
+        <Route path="/admin/projects" element={<ProjectsPage />}/>
+        <Route path="/admin/tasks" element={<TasksPage />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+// Exporting the App component as the default export
+export default App;
---
+++ Capstone_Frontend/src/pages/InquiriesPage.tsx
@@ -0,0 +1,5 @@
+const InquiriesPage = () => {
+  return <h1>Inquiries Page</h1>;
+};
+
+export default InquiriesPage;
