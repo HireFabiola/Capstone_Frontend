fd2855106157994b659068934a8e192e477ac4c9
Fabiola <fabiola.aurelien@gmail.com>
2026-06-03 12:20:32 -0400

    Added protected route logic to gatekeep dashboard from unauthorized user

---
+++ Capstone_Frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,28 @@
+// Import necessary modules and hooks from React and React Router
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+// ProtectedRoute component to restrict access to certain routes based on authentication status 
+const ProtectedRoute = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { token } = useAuth();
+
+  // If there is no authentication token, redirect the user to the admin login page
+  if (!token) {
+    return (
+      <Navigate
+        to="/admin/login"
+        replace
+      />
+    );
+  }
+
+  // If the user is authenticated, render the child components (the protected route)
+  return children;
+};
+
+// Export the ProtectedRoute component as the default export
+export default ProtectedRoute;
