f71a99f3f84a034b382a19bdd19a828b92382a13
Fabiola <fabiola.aurelien@gmail.com>
2026-06-04 09:09:56 -0400

    Added createProject code in services to use apiClient

--- Capstone_Frontend/src/services/projectService.ts
+++ Capstone_Frontend/src/services/projectService.ts
@@ -7,3 +7,15 @@ export const getProjects = async (): Promise<Project[]> => {
   const response = await apiClient.get<Project[]>("/projects");
   return response.data;
 };
+
+// Function to create a new project by sending a POST request to the API with the project data. It takes a Partial<Project> object as input, which allows for flexibility in the fields that can be included when creating a project. The function returns the created Project object as returned by the API.
+export const createProject = async (
+  projectData: Partial<Project>
+): Promise<Project> => {
+  const response = await apiClient.post<Project>(
+    "/projects",
+    projectData
+  );
+
+  return response.data;
+};
